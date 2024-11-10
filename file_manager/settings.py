92733e0a8d93c0102e316cf745b0bd0934ab0d83
Priyank Sharma <priyanksharma@Priyanks-MacBook-Air.local>
2024-11-10 22:46:44 +0530

    postgres

--- file_manager/settings.py
+++ file_manager/settings.py
@@ -72,10 +72,21 @@ WSGI_APPLICATION = 'file_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'file_manager',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
